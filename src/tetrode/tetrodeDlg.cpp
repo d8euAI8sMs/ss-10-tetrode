de178ed1cdd4ff4877bc7982779032b7ed4d81b7
kalaider <a.kalaider@yandex.ru>
2018-03-09 11:58:10 +0300

    ui: add isoline plot layer

--- src/tetrode/tetrodeDlg.cpp
+++ src/tetrode/tetrodeDlg.cpp
@@ -87,6 +87,7 @@ BOOL CTetrodeDlg::OnInitDialog()
         data.system_data.dirichlet_cell_plot,
         data.system_data.triangulation_plot,
         data.system_data.system_plot,
+        data.isoline_data.plot,
         data.system_data.point_plot
     }));
 
